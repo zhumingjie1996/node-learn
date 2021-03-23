eb1a7d6a9a963ba279c504a51f48dfd0c1ad0adc
zhumingjie1996 <1005614322@qq.com>
2021-03-23 22:58:08 +0800

    0323 commit

---
+++ 在 node.js 中从命令行接收输入/app.js
@@ -0,0 +1,23 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question(`what's your name?`, name => {
+    console.log(`hello,${name}`);
+    readline.close();
+})
+
+//使用 inquirer 更完整、更抽象
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message:`what's your name?`,
+        }
+    ])
+    .then(answer => {
+        console.log(`Hello,${answer.name}`)
+    })
